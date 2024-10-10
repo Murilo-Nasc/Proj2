7b6385a9969f44330e63a4b4c632af0cf337c536
Murilo-Nasc <163330931+Murilo-Nasc@users.noreply.github.com>
2024-10-10 00:47:09 +0000

    commit 1 - main

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-  printf("Hello World\n");
+  int opcao;
+
+  printf("BEM VINDO AO RPG\n");
+
+  while (1){
+    printf("1. Começar Sua Jornada\n2. Sair\n");
+    scanf("%d", &opcao);
+    
+    switch (opcao){
+      case 1:
+        break;
+      case 2:
+        printf("Saindo...\n");
+        return 0;
+      default:
+        printf("Opção Inválida.\n");
+        continue;
+    }
+    break;
+  }
+  
+  system("clear");
+
   return 0;
 }
